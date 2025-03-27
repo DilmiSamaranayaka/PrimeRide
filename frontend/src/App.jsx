11a298b7515a4e3e9a1eee413dfa6d0e930a22a7
DilmiSamaranayaka <128679465+DilmiSamaranayaka@users.noreply.github.com>
2025-03-27 23:14:50 +0530

    aaa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import VehicleManagementTableView from './pages/vehicleManagement';
 import VehicleManagementCreate from './components/vehicleManagement/vehicleManagementCreate/vehicleManagementCreate'
+import DriverTableView from './pages/driver';
+import DriverCreate from './components/driver/driverCreate/driverCreate';
 function App() {
   return (
     <Router>
